30e9ebb0a04ce9df607fbcce14095b04efda281a
Brijmohanlal Sahu <34767550+powerbrij007@users.noreply.github.com>
2024-03-07 11:49:28 +0530

    Supporting folders

---
+++ clientSystemFour/truffle-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    networks: {
+    development: {
+    host: "192.168.111.64",   
+    port: 8545,            
+    network_id: "*",       
+    },
+  },
+  mocha: {
+    // timeout: 100000
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.10",   
+    } 
+  },
+};
